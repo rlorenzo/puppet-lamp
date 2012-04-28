aa0f3635d128baa4122245e123c0439581afecef
Rex Lorenzo <rexlorenzo@gmail.com>
2012-04-28 00:36:48 -0700

    Make sure that clamd is running and that freshclam is called

--- manifests/classes/clamav.pp
+++ manifests/classes/clamav.pp
@@ -1,4 +1,16 @@
-# Installs needed packages to get the tex filter working for Moodle
+# Installs and setups clamav
 class clamav {
 	package { "clamd": ensure => installed }
+	
+  service {
+    clamd:
+    enable    => true,
+    ensure    => running
+  }	
+  
+  # make sure newest virus definitions are installed
+  exec { "freshclam":
+      command => "freshclam",
+      path    => "/usr/bin/",      
+  }    
 }
