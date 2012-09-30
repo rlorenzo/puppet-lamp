f5c5341dd0f7d7a46264884588d76724019ff7b5
Rex Lorenzo <rexlorenzo@gmail.com>
2012-09-30 01:56:49 -0700

    Simplified phpunit installation and added required dbunit package

--- manifests/classes/phpunit.pp
+++ manifests/classes/phpunit.pp
@@ -1,30 +1,13 @@
 # Code from:
-# https://github.com/khoomeister/puppet-devbox/blob/master/modules/lamp/manifests/init.pp
+# http://matteolandi.blogspot.com/2012/01/about-cakephp-and-vagrant.html
 class phpunit {
-	exec { '/usr/bin/pear upgrade pear':
-		require => Package['php-pear'],
-		user => root,
-	}
-
-	define discoverPearChannel {
-		exec { "/usr/bin/pear channel-discover $name":
-			onlyif => "/usr/bin/pear channel-info $name | grep \"Unknown channel\"",
-			require => Exec['/usr/bin/pear upgrade pear'],
-			user => root,
-		}
-	}
-	discoverPearChannel { 'pear.phpunit.de': }
-	discoverPearChannel { 'components.ez.no': }
-	discoverPearChannel { 'pear.symfony-project.com': }
-
-	exec { '/usr/bin/pear install pear.phpunit.de/PHPUnit':
-		onlyif => "/usr/bin/pear info phpunit/PHPUnit | grep \"No information found\"",
-		require => [
-			Exec['/usr/bin/pear upgrade pear'],
-			DiscoverPearChannel['pear.phpunit.de'],
-			DiscoverPearChannel['components.ez.no'],
-			DiscoverPearChannel['pear.symfony-project.com']
-		],
+	exec { "phpunit":
+    	command => "/usr/bin/pear upgrade pear && \
+        	        /usr/bin/pear config-set auto_discover 1 && \
+               		/usr/bin/pear install --alldeps phpunit/PHPUnit && \
+               		/usr/bin/pear install --alldeps phpunit/DbUnit",
+    	creates => "/usr/bin/phpunit",
+    	require => Package["php-pear"],
 		user => root,
 	}
 }
