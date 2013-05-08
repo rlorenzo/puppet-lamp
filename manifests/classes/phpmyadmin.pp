d07e27f9c728e600679e8f434cde157503114801
Rex Lorenzo <rex@oid.ucla.edu>
2013-05-08 16:19:41 -0700

    Fixed phpMyAdmin so that root can login without a password.

--- manifests/classes/phpmyadmin.pp
+++ manifests/classes/phpmyadmin.pp
@@ -1,21 +1,34 @@
 class phpmyadmin {
-	package { phpmyadmin: ensure => installed }  
+  #package { "phpmyadmin": ensure => installed, disablerepo => "rpmforge" }  
+  # want to use new version of phpmyadmin, but puppet doesn't
+  # yet support "disablerepo" syntax, so do it via command line
+  # http://projects.puppetlabs.com/issues/2247  
+  package { 'phpMyAdmin':
+  	require => Exec["install_phpmyadmin"]
+  }
 	
-	# make sure that config file is set
-    file { "/usr/share/phpmyadmin/config.inc.php":
-    	owner   => root,
-    	group   => root,
-    	mode    => 644,
-    	source  => "/vagrant/files/phpmyadmin/config.inc.php",
-    	require => Package["phpmyadmin"]
-  	}	  	
+  exec { "install_phpmyadmin":
+      command => "yum --disablerepo=rpmforge -y install phpMyAdmin",
+      path    => "/usr/bin/",
+      unless  => "yum list installed | grep phpMyAdmin | wc -l"
+  }  
+	
+	
+  # make sure that config file is set
+  file { "/etc/phpMyAdmin/config.inc.php":
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    source  => "/vagrant/files/phpmyadmin/config.inc.php",
+    require => Package["phpMyAdmin"]
+  }	  	
   	
-	# make sure that apache conf file is set
-    file { "/etc/httpd/conf.d/phpmyadmin.conf":
-    	owner   => root,
-    	group   => root,
-    	mode    => 644,
-    	source  => "/vagrant/files/phpmyadmin/phpmyadmin.conf",
-    	require => Package["phpmyadmin"]
-  	}  	
+  # make sure that apache conf file is set
+  file { "/etc/httpd/conf.d/phpMyAdmin.conf":
+   	owner   => root,
+   	group   => root,
+   	mode    => 644,
+   	source  => "/vagrant/files/phpmyadmin/phpMyAdmin.conf",
+   	require => Package["phpMyAdmin"]
+  }  	
 }
--- manifests/classes/srdb.pp
+++ manifests/classes/srdb.pp
@@ -4,19 +4,18 @@ class srdb {
   # need to use old version of freetds to connect, but puppet doesn't
   # yet support "disablerepo" syntax, so do it via command line
   # http://projects.puppetlabs.com/issues/2247
-  if ! defined(Package['freetds']) {
-    exec { "install_freetds":
+  exec { "install_freetds":
       command => "yum --disablerepo=remi -y install freetds",
-      path    => "/usr/bin/",      
-    }  
-  }
+      path    => "/usr/bin/",     
+      unless  => "yum list installed | grep freetds | wc -l"       
+  }  
   
   file { "/etc/freetds.conf":
       owner   => root,
       group   => root,
       mode    => 644,
       source  => "/vagrant/files/etc/freetds.conf",
-      require => [ Package["php-mssql"] ]
+      require => [ Exec["install_freetds"] ]
   }
 
   file { "/etc/odbc.ini":
