279b07b2dc1c0e58ff0d175ccc88654ddebd3811
Rex Lorenzo <rex@oid.ucla.edu>
2013-09-11 11:57:03 -0700

    Added required gem sass-css-importer-install.

--- manifests/classes/compass.pp
+++ manifests/classes/compass.pp
@@ -8,6 +8,15 @@ class compass {
     # Install compass with gem
     package { 'compass':
         ensure => latest,
-        provider => 'gem'
+        provider => 'gem',
+    	require => Package['rubygems']
     }
+	
+    # Install sass-css-importer. Need to use exec, because need to have the --pre command.
+    exec { 'sass-css-importer-install':
+    	command => '/usr/bin/gem install --pre sass-css-importer',
+    	unless => '/usr/bin/gem list -i sass-css-importer',
+    	require => Package['rubygems']
+    }
+
 }
