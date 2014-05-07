d99000b69e00674025e806f53295bd62e38dfb09
Rex Lorenzo <rex@oid.ucla.edu>
2014-05-07 12:29:46 -0700

    Updated MySQL, PHP, and phpmyadmin.

--- manifests/classes/php.pp
+++ manifests/classes/php.pp
@@ -1,6 +1,6 @@
 class php {
 
-  package { php: ensure => '5.4.27-1.el6.remi' }
+  package { php: ensure => '5.4.28-1.el6.remi' }
   
   $phppackages = [ "php-mcrypt", "php-mysql", "php-mssql", "php-odbc", "gd", "gd-devel", 
   				   "php-gd", "php-mbstring", "php-xml", "php-soap", "php-intl", "php-xmlrpc" ]
